10937943608303a131f5abb4d1b13135f84773e3
Dahye Kim <dahye.kim@nhnnext.org>
2014-04-19 15:36:46 +0900

    lab2 complete

---
+++ Lab2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Drawing
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			DrawingChar ("*", 3, 1);
+			DrawingChar ("#", 4, 2);
+			DrawingChar ("%", 8, 4);
+
+		}
+
+		public static void DrawingChar(string Character, int level, int gapDiff)
+		{
+			for (int outer = 1; outer <= level; outer++) {
+				for (int inner = 1; inner <= outer*gapDiff; inner++) {
+					Console.Write (Character);
+				}
+				Console.WriteLine ();
+			}
+			Console.WriteLine ();
+		}
+	}
+}
